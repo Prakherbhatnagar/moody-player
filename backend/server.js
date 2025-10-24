47efb251744df0e96a511db0a401ccf5dee05c7a
Prakher Bhatnagar <prakherbhatnagar63@gmail.com>
2025-10-24 18:39:23 +0530

    Delete backend directory

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-const app=require('./src/App');
-const connectDB=require('./src/db/db');
-
-connectDB();
-
-app.listen(3000,()=>{
-    console.log("server on port 3000");
-})
